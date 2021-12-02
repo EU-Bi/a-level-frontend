f41b981be0aecdf3cb17001fe94605ef8516ee4c
Eugene Bilan <en_bilan@icloud.com>
2021-12-02 18:51:16 +0200

    update

--- lecture3/script.js
+++ lecture3/script.js
@@ -1,3 +1,16 @@
 function greeting(name){
     console.log(`Hello ${name}! How are you?`);
 }
+
+
+const Lecture3 = {
+    dataType: `string, number, boolean`,
+    String: `I'm a string`,
+    Number: `2^53 - 1`,
+    Boolean: `true or false`,
+    show: function(){
+        alert(`This is ${this.dataType}`);
+    }
+
+};
+console.log(Lecture3.show);
