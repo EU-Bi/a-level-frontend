fb29969d4b5054afd2ad0954ef5cd971dfd3f691
Temu4 <artem.vorobjov@gmail.com>
2022-01-24 00:28:14 +0200

    fix hw-9

--- lecture9/task3.js
+++ lecture9/task3.js
@@ -1,18 +1,21 @@
 const calcSumOfMinNumbers = (arr) =>{
-    let min1 = getMinValue(arr);
     function getMinValue (arr){
+        // please consider to update current functionality with Math.min(...arr)
         let min = arr[0];
         for (let i = 0; i < arr.length; i++) {
             if (min > arr[i]) min = arr[i];
         }
         return min;
     }
+
+    const min1 = getMinValue(arr);
+
     const index = arr.indexOf(min1);
         if (index > -1) {
         arr.splice(index, 1);
         }
-    let min2 = getMinValue(arr);
-    let result = min1 + min2;
+    const min2 = getMinValue(arr);
+    const result = min1 + min2;
     console.log(result);   
    
 };
